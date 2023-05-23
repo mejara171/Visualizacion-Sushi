8d56cb0016508fda26006ac98931a7315ca3ecbb
Martin Zuluaga <martin.zuluaga@upb.edu.co>
2023-05-23 04:56:20 -0500

    Terminado

---
+++ Assets/Scripts/ItemManager.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class ItemManager : MonoBehaviour
+{
+    [SerializeField] GameObject sushi1, sushi2, sushi3, sushi4, sushi5;
+
+    [SerializeField] GameObject Button;
+    
+    [SerializeField] VideoClip factura;
+
+    [SerializeField] VideoManager manager;
+
+
+    private void Update()
+    {
+        SpawnItem();
+    }
+
+    public void SpawnItem()
+    {
+        if (!sushi1.gameObject.activeSelf && !sushi2.gameObject.activeSelf && !sushi3.gameObject.activeSelf && !sushi4.gameObject.activeSelf && !sushi5.gameObject.activeSelf)
+        {
+            manager.PlayVideo(factura);
+            Button.gameObject.SetActive(true);
+        }
+
+        
+    }
+
+    
+}
